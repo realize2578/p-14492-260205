375fabfc7dba6c93cf373a870ee11fbe124872bf
realize2578 <realize2578@gmail.com>
2026-02-05 15:37:27 +0900

    [feat]: 3강 실습

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner("""
+                등록
+                과거에 집착하지 마라.
+                작자미상
+                """);
+
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+        String str3 = sc.nextLine();
+
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str2);
+
+
+    }
+}
