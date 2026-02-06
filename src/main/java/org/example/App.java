e0bc27fa658e4a4afd2d5a32d38ecceede37c583
realize2578 <realize2578@gmail.com>
2026-02-06 12:23:20 +0900

    [feat]: 명언 프로그램 3단계 green and 4단계 red

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -26,6 +26,7 @@ public class App {
                 String saying = sc.nextLine();
                 System.out.print("작가 : ");
                 String author = sc.nextLine();
+                System.out.print("1번 명언이 등록되었습니다.");
             }
             case "종료" -> {
                 return;
