24b6f9e351d0cffc9424df8617960e1c2feb33ff
realize2578 <realize2578@gmail.com>
2026-02-06 11:41:15 +0900

    [feat]: 명언 프로그램 8강 blue 단계 AppRunner로 중복 제거

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,9 +1,5 @@
-import org.example.App;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,16 +8,23 @@ public class AppTest {
     @Test
     @DisplayName(" '== 명언 앱 ==' 출력")
     void t1() throws Exception{
-        Scanner sc = TestUtil.genScanner("종료");
-
-        ByteArrayOutputStream outputStream = TestUtil.setOutByteArray();
-        new App(sc).run();
-
-        String out = outputStream.toString();
+        String out = AppTestRunner.run("종료");
 
         assertThat(out).contains("== 명언 앱 ==");
     }
 
+    @Test
+    @DisplayName("등록")
+    void t2(){
+        String out = AppTestRunner.run("""
+                        등록
+                        현재를 사랑하라
+                        작자미상
+                        """);
 
+        assertThat(out).contains("== 명언 앱 ==");
+
+
+    }
 
 }
---
+++ src/test/java/AppTestRunner.java
@@ -0,0 +1,24 @@
+import org.example.App;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
+public class AppTestRunner {
+
+    public static String run(String input){
+
+        Scanner sc = TestUtil.genScanner(input);
+
+        ByteArrayOutputStream outputStream = TestUtil.setOutByteArray();
+
+        new App(sc).run();
+
+        return outputStream.toString();
+
+
+
+    }
+
+
+
+}
