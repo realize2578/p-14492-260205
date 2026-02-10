80335cc4a5e9dad654cb3461a7a181c74a90b989
남진우 <realize2578@gmail.com>
2026-02-10 11:47:24 +0900

    [feat] : 파일 저장 20단계 red

--- src/test/java/com/back/wiseSaying/controller/repository/WiseSayingFileRepositoryTest.java
+++ src/test/java/com/back/wiseSaying/controller/repository/WiseSayingFileRepositoryTest.java
@@ -2,23 +2,31 @@ package com.back.wiseSaying.controller.repository;
 
 import com.back.wiseSaying.controller.WiseSayingControllerTest;
 import org.example.global.AppContext;
+import org.example.wiseSaying.entity.WiseSaying;
 import org.example.wiseSaying.repository.WiseSayingFileRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class WiseSayingFileRepositoryTest {
     private WiseSayingFileRepository wiseSayingFileRepository;
 
-
-
-    public WiseSayingFileRepositoryTest(){
+    public WiseSayingFileRepositoryTest() {
         AppContext.init();
         wiseSayingFileRepository = AppContext.wiseSayingFileRepository;
     }
 
     @Test
     @DisplayName("명언 저장")
-    public void t1(){
+    void t1() {
+        WiseSaying wiseSaying = new WiseSaying(0,"꿈을 지녀라. 그러면 어려운 현실을 이길 수 있다.", "괴테");
+
+        wiseSayingFileRepository.save(wiseSaying);
+
+        WiseSaying foundedWiseSaying = wiseSayingFileRepository.findByIdOrNull(1);
+
+        assertThat(foundedWiseSaying).isEqualTo(wiseSaying);
 
     }
 }
